c421ce7daa6c92de2b937c505f9ca0cb793c6d1b
andreredesti <72885977+andreredesti@users.noreply.github.com>
2023-05-02 09:10:08 -0300

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-//import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'my-app',
